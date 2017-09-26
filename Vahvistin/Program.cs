c6b48c57894bd5e0afa96cf2a001c2f7483fc337
Kylis <H9109@student.jamk.fi>
2017-09-26 22:00:50 +0300

    did exercises 1-3...

---
+++ Vahvistin/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vahvistin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vahvistin Kajari = new Vahvistin();
+
+            do
+            {
+
+
+                Console.WriteLine("Anna äänenvoimakkuus.");
+                string sisaantulo = Console.ReadLine();
+                int luku;
+                bool valinta = int.TryParse(sisaantulo, out luku);
+                if (valinta) { Kajari.Aani = luku; }
+
+                else
+                {
+                    break;
+                }
+                Console.WriteLine("Äänenvoimakkuus: {0} ", Kajari.Aani);
+            } while (true);
+        }
+    }
+}
---
+++ Vahvistin/Vahvistin.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vahvistin
+{
+    class Vahvistin
+    {
+        private const int maksimi = 100;
+        private int aani;
+        public int Aani
+        {
+
+            get { return aani; }
+            set
+            {
+                if (value > maksimi)
+                    aani = maksimi;
+                else if (value < 0)
+                    aani = 0;
+                else
+                    aani = value;
+            }
+        }
+
+        }
+    }
