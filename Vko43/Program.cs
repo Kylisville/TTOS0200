6d1b779336a980dfdd2466b4f1b22fbe9ca43cfb
ville <H9109@student.jamk.fi>
2017-10-31 21:40:22 +0200

    did final assignment using lecturer's example

--- Vko43/Program.cs
+++ Vko43/Program.cs
@@ -4,6 +4,8 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace Vko43
 {
@@ -47,11 +49,31 @@ namespace Vko43
             /*   People nimet = new People();
                nimet.LaskeRivit();
                nimet.LaskeNimet(); */
-                 Numbers numbers = new Numbers();
-                 numbers.Inputs(); 
-                 numbers.AddToTxt();
-                 numbers.ReadTxt();
- 
+            /*      Numbers numbers = new Numbers();
+                  numbers.Inputs(); 
+                  numbers.AddToTxt();
+                  numbers.ReadTxt();*/
+            try
+            {
+                List<Ohjelmat> ohjelmat = new List<Ohjelmat>();
+                ohjelmat.Add(new Ohjelmat { Nimi = "Science of Stupid", Kanava = "National Geographic", Aika = "00:00 - 00:30", Info = "Typerät videot -sarjassa tieteen asiantuntijat kertovat, miksi jokin harmittomalta tuntuva temppu voi johtaa onnettomuuteen." });
+                ohjelmat.Add(new Ohjelmat { Nimi = "Astral-TV", Kanava = "YLE TV2", Aika = "04:05 - ", Info = "Vastaanota neuvoja parhailta asiantuntijoilta! -verkossa ennustajat & ennustaminen" });
+                Stream stream = new FileStream(@"C:\Users\ville\desktop\ohjelmat.txt",FileMode.Create,FileAccess.Write,FileShare.None);
+                IFormatter formatter = new BinaryFormatter();
+                formatter.Serialize(stream, ohjelmat);
+                stream.Close();
+                Stream openStream = new FileStream(@"C:\Users\ville\desktop\ohjelmat.txt", FileMode.Open, FileAccess.Read, FileShare.Read);
+                List<Ohjelmat> lueohjelma = (List<Ohjelmat>)formatter.Deserialize(openStream);
+                openStream.Close();
+                foreach (Ohjelmat p in lueohjelma)
+                {
+                    Console.WriteLine("Ohjelma: {0}\n Kanava: {1}\n Aika {2}\n Info: {3}",p.Nimi,p.Kanava,p.Aika,p.Info);
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
 
         }
     }
---
+++ Vko43/ohjelmat.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vko43
+{
+    [System.Serializable]
+    class Ohjelmat
+    {
+        public string Nimi { get; set; }
+        public string Kanava { get; set; }
+        public string Aika { get; set; }
+        public string Info { get; set; }
+
+    }
+}
