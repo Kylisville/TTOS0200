1d8db93a6e1822ef87888b7c39e631b4c7d3a6ac
ville <H9109@student.jamk.fi>
2017-11-12 14:16:54 +0200

    did exercises 1 and 2

--- CurrencyConverter/MainWindow.xaml.cs
+++ CurrencyConverter/MainWindow.xaml.cs
@@ -24,10 +24,34 @@ namespace CurrencyConverter
         {
             InitializeComponent();
         }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Convert_Click(object sender, RoutedEventArgs e)
         {
-
+            //tarkastetaan onko syöttö luku
+            bool check = double.TryParse(Amount.Text, out double d);
+            if (!check)
+                {
+                ErrorBox.Text = "Invalid input. Please enter numbers only.";
+                return;
+                }
+            ErrorBox.Text = String.Empty;
+       
+            if (Combo1.SelectedIndex == 0 && Combo2.SelectedIndex == 1)
+                {
+                
+                //Muunnetaan eurot dollareiksi
+                double t = d / 0.8997;
+                ConvertedAmount.Text = "Converted Amount: " + t.ToString("0.00") + "\t $";
+            }
+            else if (Combo1.SelectedIndex == 1 && Combo2.SelectedIndex == 0)
+            {
+                //muunnetaan dollarit euroiksi
+                double t = d * 0.8997;
+                ConvertedAmount.Text = "Converted Amount: " + t.ToString("0.00") + "\t €";
+            }
+            else
+            {
+                ConvertedAmount.Text = "You can't convert same currency!";
+            }
         }
     }
 }
