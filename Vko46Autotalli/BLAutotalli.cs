9994523cc0567630c65c8711a5729d92c8846472
H9109 <H9109@student.jamk.fi>
2017-11-15 11:06:17 +0200

    asd

---
+++ Vko46Autotalli/BLAutotalli.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vko46Autotalli
+{
+    class BLAutotalli
+    {
+
+    }
+    public static class Autotalli
+    {
+        public static List<Auto> HaeAutot()
+        {
+            List<Auto> autot = new List<Auto>();
+            //GUIn testaamista varten keksitään muutama auto
+            Auto a = new Auto();
+            a.Merkki = "Volvo";
+            a.Malli = "V70";
+            a.VM = 2007;
+            a.KM = 200000;
+            a.Hinta = 9000F;
+            a.URL = "VolvoV70.png";
+            autot.Add(a);
+            //toinenkin auto
+            Auto b = new Auto() { Merkki = "Audi", Malli = "A4", VM = 2010, KM = 100000, Hinta = 19900, URL="AudiA4.png" };
+            autot.Add(b);
+            //kolmaskin
+            Auto c = new Auto() { Merkki = "Saab", Malli = "99", VM = 2000, KM = 1000000, Hinta = 900 };
+            autot.Add(c);
+            Auto v = new Auto() { Merkki = "Volvo", Malli = "240", VM = 1992, KM = 130000, Hinta = 1500, URL="240.png" };
+            autot.Add(v);
+            return autot;
+        }
+    }
+    public class Auto
+    {
+        public string Merkki { get; set; }
+        public string Malli { get; set; }
+        public int VM { get; set; }
+        public int KM { get; set; }
+        public float Hinta { get; set; }
+        public string URL { get; set; }
+      
+    }
+}
---
+++ Vko46Autotalli/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Vko46Autotalli
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Auto> autot;
+        private const string polku = @"D:\\H9109\\";
+        public MainWindow()
+        {
+            //tänne koodi joka suoritetaan ikkunana avauksen luonnissa.
+            InitializeComponent();
+            NaytaKuva("autotalli.png");
+        }
+
+        private void btnHaeAutot_Click(object sender, RoutedEventArgs e)
+        {
+            //pyydetään BL-kerrokselta autot, ja näytetään käyttäjälle
+            autot = Autotalli.HaeAutot();
+            imgAutot.ItemsSource = autot;
+        }
+        private void NaytaKuva(string url)
+        {
+            try
+            {
+
+                if (string.IsNullOrEmpty(url))
+                {
+                    url = "puuttuu.png";
+                }
+                url = polku + url;
+                //kuvan näyttö
+                BitmapImage pic = new BitmapImage();
+                pic.BeginInit();
+                pic.UriSource = new Uri(url);
+                pic.EndInit();
+                imgAuto.Stretch = Stretch.Fill;
+                imgAuto.Source = pic;
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void imgAutot_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //huom, olemme itse populoineet DataGridin Auto-olioilla, joten kukin  DatGridin jäsen/alkio on itseasiassa auto-olio.
+            object obj = imgAutot.SelectedItem;
+            if (obj != null)
+            {
+                Auto valittu = (Auto)obj;
+                NaytaKuva(valittu.URL);
+            }
+        }
+
+        private void btnHaeAudit_Click(object sender, RoutedEventArgs e)
+        {
+            //näkyviin pelkästään Audi-merkkiset autot
+            var result = autot.Where(m => m.Merkki.Contains("Audi"));
+            imgAutot.ItemsSource = result;
+        }
+    }
+}
