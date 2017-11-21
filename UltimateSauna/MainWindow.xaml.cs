7b055e6dd56318006941d7828b21602ee97ce18d
ville <H9109@student.jamk.fi>
2017-11-21 17:57:10 +0200

    Done all 4 exercises

---
+++ UltimateSauna/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UltimateSauna
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void btn_Click(object sender, RoutedEventArgs e)
+        {
+            InfoBox.Text = "";
+            string buttonString = (((Button)sender).Content).ToString();
+            bool isitNumber = double.TryParse(buttonString, out double number);
+            if (isitNumber || buttonString == ".")
+            {
+                Input.Text += buttonString;
+            }
+            else if (buttonString == "<-" && Input.Text.Length > 0)
+            {
+                Input.Text = Input.Text.Substring(0, Input.Text.Length - 1);
+            }
+            else if (buttonString == "ok")
+            {
+                if ((bool)RadioHumi.IsChecked || (bool)RadioTemp.IsChecked)
+                {
+                    double input;
+                    bool isitDouble = double.TryParse(Input.Text, out input);
+                    if (!isitDouble)
+                    {
+                        InfoBox.Text = "Invalid input";
+                    }
+                    if ((bool) RadioHumi.IsChecked)
+                    {
+                        if (input > 100)
+                        {
+                            InfoBox.Text = "You can't go above maximum (100)";
+                            Input.Text = String.Empty;
+                        }
+                        else
+                        {
+                           
+                            Humi.Text = input.ToString("N2");
+                            Input.Text = String.Empty;
+                        }
+                    }
+                    else if ((bool)RadioTemp.IsChecked)
+                    {
+                        if (input > 120)
+                        {
+                            InfoBox.Text = "You can't go above maximum(120)";
+                            Input.Text = String.Empty;
+                        }
+                        else
+                        {
+                            Temp.Text = input.ToString("N2");
+                            Input.Text = String.Empty;
+                        }
+                    }
+                }
+                else
+                {
+                    InfoBox.Text = "Invalid input";
+                }
+            }
+        }
+    }
+}
---
+++ WPFLottery/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPFLottery;
+
+namespace WPFLottery
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// Lotto: arvotaan 7 numeroa väliltä 1-40
+    /// Viking Lotto: arvotaan 6 numeroa 1-48
+    ///Eurojackpot: arvotaan 5 numeroa 1-50 ja ja 2 tähtinumeroa väliltä 1-10
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeGames();
+        }
+        private void InitializeGames()
+        {
+            Game.Items.Add(new Lotto());
+            Game.Items.Add(new Vikinglotto());
+            Game.Items.Add(new Eurojackpot());
+        }
+        private void DrawBtn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (Game.SelectedIndex >= 0)
+                {
+                    Lottery game = (Lottery)Game.SelectedItem;
+                    Results.Text = "";
+                    int number = int.Parse(DrawAmt.Text);
+                    for (int x = 0; x < number; x++)
+                    {
+                        game.Generate();
+                        Results.Text += String.Format("Rivi {0}: ", x + 1) + game.LineToString() + "\n";
+                        game.Numbers.Clear();
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                Results.Text += "Please enter the amount of lines";
+            }
+        }
+        private void ClearBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Results.Text = String.Empty;
+            DrawAmt.Text = String.Empty;
+            Game.SelectedIndex = -1;
+        }
+    }
+}
